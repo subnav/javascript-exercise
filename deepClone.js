96088b066a34282fe9bd1642601112e977737ed9
subnav <2696356229@qq.com>
2023-04-24 12:45:18 +0800

    深拷贝

---
+++ deepClone.js
@@ -0,0 +1,24 @@
+/**
+ * 深拷贝
+ */
+
+function deepClone(obj = {}, map = new Map) {
+  if (typeof obj !== 'object') return func;
+
+  //使用 map 防止循环引用栈溢出
+  if (map.get(obj)) return map.get(obj);
+
+  let result = {};
+  if (obj instanceof Array) {
+    result = [];
+  }
+  map.set(obj, result);
+
+  for (let key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      result[key] = deepClone(obj[key], map);
+    }
+  }
+
+  return result;
+}
